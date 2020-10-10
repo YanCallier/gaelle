ea238a717c9c67aa34831fb1d39cd24cbc3c38ce
Yan Callier <yan.callier@gmail.com>
2020-10-10 20:50:42 +0200

    for in render + vue methods

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-var currentQuestion = 0;
-var test = 0;
-
-function resolve(answer) {
-    app.answer = answer;
-    app.answerTxt = app.content.answers[answer].text;
-    if (app.content.answers[answer].id == app.content.goodAnswer) {
-        currentQuestion++;
-        app.nextTxt = "SUIVANT >>";
-    } else {
-        app.nextTxt = "ESSAYES ENCORE >>";
-    }
-}
-
-function nextQuestion() {
-    if (currentQuestion >= data.length) {
-        currentQuestion = 0;
-    }
-    app.content = data[currentQuestion];
-    app.answer = null;
-}
-
 var app = new Vue({
     el: '#app',
     data: {
-        content: data[currentQuestion],
+        currentQuestion: 0,
+        content: data[0],
         answer: null,
-    }
+    },
+    methods: {
+        resolve: function (answer, answerName) {
+            this.answer = answerName;
+            this.answerTxt = answer.text;
+            if (answerName === this.content.goodAnswer) {
+                this.currentQuestion++;
+                this.nextTxt = "SUIVANT >>";
+            } else {
+                this.nextTxt = "ESSAYES ENCORE >>";
+            }
+        },
+        nextQuestion: function () {
+            if (this.currentQuestion >= data.length) {
+                this.currentQuestion = 0;
+            }
+            this.content = data[this.currentQuestion];
+            this.answer = null;
+        }
+    },
 });
